fcbdece60ec6a40daee670fd7c107b72c8d02119
christopherszalaty <krzysztofszalaty@gmail.com>
2025-08-05 14:33:16 +0200

    Zmiany w frontendzie

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,24 @@
-def main():
-    print("Hello from backend!")
+# import os 
+# import uuid
+# from typing import Annotated, TypedDict, List
 
-def drukuj():
-    print("drukuj")
+# from dotenv import load_dotenv
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# from pydantic import BaseModel
+# from sse_starlette.sse import EventSourceResponse
+# from langchain_tavily import TavilySearch
 
-if __name__ == "__main__":
-    main()
-    drukuj()
+# Import relevant functionality
+from langchain.chat_models import init_chat_model
+from langchain_tavily import TavilySearch
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+memory = MemorySaver()
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature=0, streaming=True)
+search = TavilySearch(max_results=2)
+tools = [search]
+agent_executor = create_react_agent(model, tools, checkpointer=memory)
+
